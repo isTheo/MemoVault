b4e67a5277dc8d2abeea03194989f8ae9e71575e
isTheo <matteohg07@gmail.com>
2024-05-18 23:46:35 +0200

    Initial Commit

---
+++ MemoVault/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MemoVault
+//
+//  Created by Matteo Orru on 18/05/24.
+//
+/*
+ 
+The table view controller lists notes.
+Tapping on a note should slide in a detail view controller that contains a full-screen text view.
+ Notes should be loaded and saved using Codable.
+ Add a toolbar items to the detail view controller like “delete” and “compose” and an action button to the navigation bar in the detail view controller that shares the text using UIActivityViewController.
+ */
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
